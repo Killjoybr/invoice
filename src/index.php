32202f5091a8e744a253731ec3e0f23f17dc9949
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2026-02-12 16:43:24 -0300

    add jquerymask

--- src/index.php
+++ src/index.php
@@ -8,30 +8,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invoice Form</title>
     <link rel="stylesheet" href="../vendor/classless/classless.css">
+    <script src="https://code.jquery.com/jquery-4.0.0.min.js" integrity="sha256-OaVG6prZf4v69dPg6PhVattBXkcOWQB62pdZ3ORyrao=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 </head>
 <body>
     <header>
         <nav class="noprint">
             <ul>
                <li>Formulário</li>
-               <li>teste</li>
-               <li>teste</li>
-               <li>teste</li>
+               <li>lorem</li>
+               <li>ipsum</li>
+               <li>dolor</li>
             </ul>
         </nav>
     </header>
     <main>
         <form action="./modelos/form_br.php" method="post">
-            <label for="nome_empresa">Nome da Empresa</label>
-            <input type="text" name="nome_empresa" id="nome_empresa">
-            <label for="cnpj">CNPJ</label>
-            <input type="number" name="cnpj" id="cnpj">
-            <label for="telefone">Telefone</label>
-            <input type="tel" name="telefone" id="telefone">
-            <label for="endereco">Endereço</label>
-            <input type="text" name="endereco" id="endereco">
-            <label for="periodo_contrato">Período do Contrato</label>
-            <input type="text" name="periodo_contrato" id="periodo_contrato">
+            <div class=col>
+                <div class="row">
+                <fieldset>
+                    <legend>Informações da Empresa</legend>
+                    <label for="nome_empresa">Nome da Empresa</label>
+                    <input type="text" name="nome_empresa" id="nome_empresa">
+                    <label for="cnpj">CNPJ</label>
+                    <input type="text" name="cnpj" id="cnpj">
+                    <label for="telefone">Telefone</label>
+                    <input type="tel" name="telefone" id="telefone">
+                    <label for="endereco">Endereço</label>
+                    <input type="text" name="endereco" id="endereco">
+                </fieldset>
+                <fieldset>
+                    <legend>Informações do Cliente</legend>
+                    <label for="razao_cliente">Empresa Cliente</label>
+                    <input type="text" name="razao_cliente" id="razao_cliente">
+                    <label for="cnpj_cliente">CNPJ</label>
+                    <input type="text" name="cnpj_cliente" id="cnpj_cliente">
+                </fieldset>
+                </div>
+                <fieldset>
+                    <legend>Informações de Produto</legend>
+                    <label for="periodo_contrato">Período do Contrato</label>
+                    <input type="text" name="periodo_contrato" id="periodo_contrato">
+                </fieldset>
+                <fieldset>
+                    <legend>Informações de Pagamento</legend>
+                    <label for="beneficiario">Beneficiário</label>
+                    <input type="text" name="beneficiario" id="beneficiario">
+                    <label for="agencia">Agência</label>
+                    <input type="text" name="agencia" id="agencia">
+                    <label for="numero_conta">Número da Conta</label>
+                    <input type="text" name="numero_conta" id="numero_conta">
+                    <label for="cnpj_beneficiario">CNPJ</label>
+                    <input type="text" name="cnpj_beneficiario" id="cnpj">
+                </fieldset>
+            </div>
             <button type="submit">Enviar</button>
         </form>
     </main>
@@ -44,4 +74,9 @@
             <a href="https://github.com/ThiagoAbrahao" target="_blank" rel="noopener noreferrer">Thiago</a>
     </footer>
 </body>
+<script>
+    $(document).ready(function(){
+    $('#cnpj').mask('99.999.999/9999-99');
+});
+</script>
 </html>
